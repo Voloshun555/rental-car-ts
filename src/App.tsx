5335b7f856aba263ad8b9b073465fa9091f9277b
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2023-11-02 13:29:29 +0200

    added layout routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./Component/Layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import "./App.css";
 
-function App() {
+const Home = lazy(() => import("./Pages/Home"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
   );
-}
+};
 
 export default App;
