f9fd80b3d259b8b810c40c65fd4bbd91567c0507
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2023-11-05 16:16:04 +0200

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "./Component/Layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
-import "./App.css";
+import "../src/scss/_reset.scss"
+import {  useAppSelector, useAppDispatch } from "./hooks/Hooks"
+import { fetchCars } from "./redux/carsSlice/operations";
 
 const Home = lazy(() => import("./Pages/Home"));
+const Catalogue = lazy(() => import("./Pages/Catalogue"));
+const Favorites = lazy(() => import("./Pages/Favorites"));
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+
+  const cars = useAppSelector((state) => state.cars.items);
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalogue data={cars} />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
